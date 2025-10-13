b92df7288681dd8a4160762f8f2605a1d65afda7
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-10-13 07:25:14 +0300

    update validation

--- src/modules/reviews/review.validation.js
+++ src/modules/reviews/review.validation.js
@@ -2,7 +2,7 @@ import  joi from 'joi';
 import { generalFields } from '../../middelwares/validate.js';
 
 export const addReviewVal = joi.object({
-    comment : generalFields.comment.required(),
+    comment : generalFields.comment,
     rate: generalFields.rate.required(),
     product : generalFields.objectId.required(),
 });
